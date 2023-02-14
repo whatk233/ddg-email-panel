5a455138cedfbefdbb92486fa425dd58398f5944
Whatk <i@whatk.me>
2023-02-14 14:19:55 +0800

    fix: Login request (#3) refactor: Remove node-fetch dependency

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,3 @@
 export const DUCKDUCKGO_API_ENDPOINT = 'https://quack.duckduckgo.com/api'
+export const DUCKDUCKGO_API_USERAGENT = "Mozilla/5.0 (Linux; Android 12) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/109.0.5414.86 Mobile DuckDuckGo/5 Safari/537.36"
 export const USERNAME_REGEX = /^[A-Za-z0-9]+$/
